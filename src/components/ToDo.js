ef05f55f159eab6a890049786aee0c7edaf3f42b
Vince Cimino <vince_cimino@Vinces-MacBook-Air.local>
2019-12-07 13:02:05 -0800

    todo

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -9,15 +9,15 @@ class ToDo extends React.Component {
 
                 <div className="to-do-part">
                     <Header header="To do" color='#26262F' />
-                    {this.props.project.stage3.to_do.map((item, index) => <Card isDone={false} isWorking={false} item={item} />)}
+                    {this.props.project.stage3.to_do.map((item, index) => <Card isDone={false} isWorking={false} item={item} key={index} />)}
                 </div>
                 <div className="doing-part">
                     <Header header="In progress" color='#26262F' />
-                    {this.props.project.stage3.in_progress.map((item, index) => <Card isDone={false} isWorking={true} item={item} />)}
+                    {this.props.project.stage3.in_progress.map((item, index) => <Card isDone={false} isWorking={true} item={item} key={index}  />)}
                 </div>
                 <div className="done-part">
                     <Header header="Done" color='#26262F' />
-                    {this.props.project.stage3.done.map((item, index) => <Card isDone={true} isWorking={false} item={item} />)}
+                    {this.props.project.stage3.done.map((item, index) => <Card isDone={true} isWorking={false} item={item} key={index} />)}
                 </div>
             </div>
         )
